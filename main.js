40b2f8e7ffe10ad791354aefdeed7aa1e5c5d92e
JassOrt <108296488+JassOrt@users.noreply.github.com>
2023-04-06 23:50:21 -0600

    Funcionalidad PWA

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-if('serviceWorker' in navigator){
-    console.log("Puedes usar el service worker");
-    // Configuracion del sw 
-    navigator.serviceWorker.register('./sw.js')
-                            .then(res => console.log('SW cargado correctamente', res))
-                            .catch(err => console.log('serviceWorker no se ha podido registrar', err));
-}
-else
+//cargar Service Worker
+if('serviceWorker' in navigator)
 {
-    console.log("No se puede usar el service worker")
+    console.log("Puedes usar el service worker");
+
+    navigator.serviceWorker.register('./sw.js')
+                    .then(res => console.log("SW cargado correctamente",res))
+                    .catch(err => console.log("Service Worker No se ha podido registrar",err));
+
+}else{
+    console.log("No se puede usar el service worker");
 }
